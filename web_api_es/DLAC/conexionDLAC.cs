c1588f7fd617761a50316c2e402ae5c3dc4ac9fc
kair069 <81661845+kair069@users.noreply.github.com>
2023-10-13 18:48:29 -0500

    Update conexionDLAC.cs

--- web_api_es/DLAC/conexionDLAC.cs
+++ web_api_es/DLAC/conexionDLAC.cs
@@ -3,7 +3,7 @@ namespace web_api_es.DLAC
 {
     public class conexionDLAC
     {
-        SqlConnection cn = new SqlConnection(@"server=LAPTOP-ET9O9DGS; database=Plataforma2022;Trusted_Connection= True;" +
+        SqlConnection cn = new SqlConnection(@"server=servidoralex.database.windows.net; database=Plataforma2022;Trusted_Connection= True;" +
             "MultipleActiveResultSets= True;TrustServerCertificate= False;Encrypt= False");
         public SqlConnection getcn
         {
